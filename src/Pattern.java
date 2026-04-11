3e2304488232a15663f4aaa0d4e92737e54fed04
Sumit Rathore <sumitrathore45528@gmail.com>
2026-04-12 02:19:03 +0530

    Pattern printing

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Pattern {
+    static void main() {
+       /*  // Print SOLID SQUARE pattern
+        int n = 4;
+
+        for (int row = 1; row<=n; row++){
+            //for wach row -> n columns
+            for (int col = 1; col<=n; col++){
+                //print star
+                System.out.print("*");
+            }
+            //move to next line or row
+            System.out.println();
+        } */
+
+        // Print SOLID SQUARE pattern
+
+    }
 }
